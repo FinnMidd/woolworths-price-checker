927adab85abc34780db325568f3bfb5e2a62d36d
Finn Middleton <92194155+FinnMidd@users.noreply.github.com>
2024-06-18 23:12:40 +1000

    Update: upload current price_checker script

--- price_checker.py
+++ price_checker.py
@@ -1,6 +1,10 @@
 import json
 from playwright.sync_api import sync_playwright
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
+# Function for getting prices from webpage
 def get_price(url):
     with sync_playwright() as p:
         browser = p.firefox.launch()
@@ -31,13 +35,16 @@ def get_price(url):
 
     return price
 
+# Function for loading items from JSON file
 def load_items(filename='items.json'):
     with open(filename, 'r') as f:
         return json.load(f)
 
-def check_prices():
+# Function for checking prices and running email function
+def check_prices_and_notify():
     items = load_items()
     prices = set()
+    price_change_detected = False
     
     for item in items:
         current_price = get_price(item['url'])
@@ -45,6 +52,7 @@ def check_prices():
             prices.add(current_price)
         if current_price != item['price']:
             print(f"Price change detected for {item['name']}: {item['price']} -> {current_price}")
+            price_change_detected = True
 
 if __name__ == "__main__":
-    check_prices()
+    check_prices_and_notify()
